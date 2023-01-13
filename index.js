059f26b57fd471afaa4520ca8f79ab3ef0ff7252
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-01-12 20:59:06 -0600

    completed labwork

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(arr) {
+    return arr.slice(0, 2);
+}
+
+function returnLastTwoDrivers(arr) {
+    return arr.slice((arr.length - 2, 2));
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(x){
+    return (function() {
+        return x * x;
+    })
+}
+
+function fareDoubler(fare) {
+    return fare * 2;
+}
+
+function fareTripler(fare) {
+    return fare * 3;
+}
+
+function selectDifferentDrivers(arrayOfDrivers, functionChoice) {
+    return functionChoice(arrayOfDrivers);
+}
